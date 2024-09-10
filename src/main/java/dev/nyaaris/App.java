b1d3e491e55c75c98e0216b32bf7c088a102e96a
Antoni Felczyński <felczynskiantoni@gmail.com>
2024-09-10 23:37:40 +0200

    Constrain board to n*n. Game state checking

--- src/main/java/dev/nyaaris/App.java
+++ src/main/java/dev/nyaaris/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        Board board_model = new Board(3,3);
+        Board board_model = new Board(3);
         BoardView board_view = new BoardView();
         BoardController controller = new BoardController(board_model, board_view);
     }
--- src/main/java/dev/nyaaris/Board.java
+++ src/main/java/dev/nyaaris/Board.java
@@ -1,46 +1,128 @@
 package dev.nyaaris;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 public class Board {
-    private enum Player {
+    public enum Player {
         NOUGHT, CROSS
     }
 
-    private class Field {
-        private Optional<Player> filled_by = Optional.empty();
+    public enum GameState {
+        ONGOING, DRAW, NOUGHT_WON, CROSS_WON
+    }
+
+    public static class Field {
+        private Optional<Player> filledBy = Optional.empty();
+
+        public Optional<Player> getFilledBy() {
+            return filledBy;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof Field) return filledBy.equals(((Field) obj).filledBy);
+            return false;
+        }
+
+        @Override
+        public int hashCode() {
+            return filledBy.hashCode();
+        }
     }
 
     private final Field[][] fields;
+    private final List<Field[]> winConditionRows;
 
-    public Board(int width, int height) {
-        fields = new Field[width][height];
-    }
-
-    public boolean isGameFinished() {
-        return false;
-    }
-
-    public Player whoMovesNext(){
-        int moves = 0;
+    public Board(int boardSize) {
+        fields = new Field[boardSize][boardSize];
         for (Field[] fields2 : fields) {
-            for (Field field : fields2) {
-                if (field.filled_by.isPresent()){
-                    moves++;
-                }
+            Arrays.setAll(fields2, (i) -> new Field());
+        }
+        winConditionRows = getWinConditionRows();
+    }
+
+    public Field[][] getBoardState() {
+        Field[][] returnArray = new Field[fields.length][];
+        for (int i = 0; i < returnArray.length; i++) {
+            returnArray[i] = fields[i].clone();
+        }
+        return returnArray;
+    }
+
+    public GameState getGameState() {
+        for (Field[] row : winConditionRows) {
+            Optional<Player> winningPlayer = checkRowWins(row);
+            if (winningPlayer.isPresent()) {
+                return winningPlayer.get() == Player.NOUGHT ? GameState.NOUGHT_WON : GameState.CROSS_WON;
             }
         }
-        return moves % 2 == 0 ? Player.NOUGHT : Player.CROSS;
+        if (getMovesDone() == fields.length * fields.length) {
+            return GameState.DRAW;
+        }
+        return GameState.ONGOING;
     }
 
-    public boolean move(int x, int y){
+    public Player whoMovesNext() {
+        return getMovesDone() % 2 == 0 ? Player.NOUGHT : Player.CROSS;
+    }
+
+    public boolean move(int x, int y) {
         Field field = fields[x][y];
-        if (field.filled_by.isPresent()){
+        if (field.filledBy.isPresent()) {
             return false;
-        }
-        else{
-            field.filled_by = Optional.of(whoMovesNext());
+        } else {
+            field.filledBy = Optional.of(whoMovesNext());
             return true;
         }
     }
+
+    private int getMovesDone() {
+        int movesDone = 0;
+        for (Field[] fieldRow : fields) {
+            for (Field field : fieldRow) {
+                if (field.filledBy.isPresent()) {
+                    movesDone++;
+                }
+            }
+        }
+        return movesDone;
+    }
+
+    private List<Field[]> getWinConditionRows() {
+        List<Field[]> result = new ArrayList<>();
+        for (int i = 0; i < fields.length; i++) {
+            result.add(fields[i]);
+            Field[] row = new Field[fields.length];
+            for (int j = 0; j < fields.length; j++) {
+                row[j] = fields[j][i];
+            }
+            result.add(row);
+        }
+        Field[] diagonal1 = new Field[fields.length];
+        Field[] diagonal2 = new Field[fields.length];
+        for (int i = 0; i < fields.length; i++) {
+            diagonal1[i] = fields[i][i];
+            diagonal2[i] = fields[i][fields.length - 1 - i];
+        }
+        result.add(diagonal1);
+        result.add(diagonal2);
+        return result;
+    }
+
+    private Optional<Player> checkRowWins(final Field[] row) {
+        Optional<Player> pretender = row[0].filledBy;
+        if (pretender.isPresent()) {
+            for (int i = 1; i < row.length; i++) {
+                if (row[i].filledBy != pretender) {
+                    continue;
+                }
+                return pretender;
+            }
+        }
+        return Optional.empty();
+    }
+
 }
